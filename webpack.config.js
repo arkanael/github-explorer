b403c0633247ecc5b809e2fabe1f30306c096257
arkanael <arkanael@gmail.com>
2021-10-16 22:39:30 -0300

    Feito a configuração do devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
-  output:{
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
   },
   resolve: {
-        extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
+  devServer:{
+     static: path.join(__dirname, "public"),
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    })
+  ],
   module: {
-        rules: [
-              {
-                    test: /\.jsx$/,
-                    exclude: /node_modules/,
-                    use: 'babel-loader',
-              }
-        ]
-  }
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
 };
